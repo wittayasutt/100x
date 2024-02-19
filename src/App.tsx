01ee98e0c6cd0b08c84865b64a72c289294b3ce0
minapapol <min@100x.fi>
2024-02-19 20:35:18 +0700

    refactor/style code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
-import "./App.css";
-import { useSubPythPrices } from "./hooks/useSubPythPrices";
-import { TokenTable } from "components/TokenTable";
-import { LikedList } from "components/LikedList";
-import { LikedTokenReport } from "components/LikedTokenReport";
-import { tokens } from "./constants";
+import { useState } from 'react';
+import './App.css';
+import { useSubPythPrices } from './hooks/useSubPythPrices';
+import { TokenTable } from 'components/TokenTable';
+import { LikedList } from 'components/LikedList';
+import { LikedTokenReport } from 'components/LikedTokenReport';
+import { tokens } from './constants';
 
 function App() {
-  const [likedList, setLikedList] = useState<string[]>([]);
-  const [tokenPrices, previousTokenPrices] = useSubPythPrices();
+	const [likedList, setLikedList] = useState<string[]>([]);
+	const [tokenPrices, previousTokenPrices] = useSubPythPrices();
 
-  return (
-    <div className="App">
-      <div style={{ margin: "4rem", textAlign: "center" }}>
-        <header className="App-header">
-          Token prices from {tokens.length} tokens
-        </header>
-        <LikedTokenReport likedTokens={likedList} tokenPrices={tokenPrices} />
-      </div>
-      <div className="App-content">
-        <TokenTable
-          tokenPrices={tokenPrices}
-          previousPrices={previousTokenPrices}
-          onChange={setLikedList}
-        />
-        <LikedList tokens={likedList} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div style={{ margin: '4rem', textAlign: 'center' }}>
+				<header className='App-header'>Token prices from {tokens.length} tokens</header>
+				<LikedTokenReport likedTokens={likedList} tokenPrices={tokenPrices} />
+			</div>
+			<div className='App-content'>
+				<TokenTable tokenPrices={tokenPrices} previousPrices={previousTokenPrices} onChange={setLikedList} />
+				<LikedList tokens={likedList} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/hooks/useSubPythPrices.ts
+++ src/hooks/useSubPythPrices.ts
@@ -1,63 +1,53 @@
-import { BigNumber } from "@ethersproject/bignumber";
-import { EvmPriceServiceConnection, PriceFeed } from "@pythnetwork/pyth-evm-js";
-import { tokens } from "../constants";
-import { useEffect, useState } from "react";
+import { BigNumber } from '@ethersproject/bignumber';
+import { EvmPriceServiceConnection, PriceFeed } from '@pythnetwork/pyth-evm-js';
+import { tokens } from '../constants';
+import { useEffect, useState } from 'react';
 
-export const useSubPythPrices = (): [
-  Record<string, BigNumber>,
-  Record<string, BigNumber>
-] => {
-  const [previousPriceFeed, setPreviousPriceFeed] = useState<
-    Record<string, BigNumber>
-  >({});
-  const [priceFeed, setPriceFeed] = useState<Record<string, BigNumber>>({});
+export const useSubPythPrices = (): [Record<string, BigNumber>, Record<string, BigNumber>] => {
+	const [previousPriceFeed, setPreviousPriceFeed] = useState<Record<string, BigNumber>>({});
+	const [priceFeed, setPriceFeed] = useState<Record<string, BigNumber>>({});
 
-  useEffect(() => {
-    const connection = new EvmPriceServiceConnection(
-      "https://hermes.pyth.network"
-    );
+	useEffect(() => {
+		const connection = new EvmPriceServiceConnection('https://hermes.pyth.network');
 
-    connection.subscribePriceFeedUpdates(
-      tokens.map((t) => t.priceId),
-      (feed) => {
-        const tokenName = tokens.find(
-          (t) => t.priceId.toLowerCase() === "0x".concat(feed.id)
-        )!.name;
+		connection.subscribePriceFeedUpdates(
+			tokens.map((t) => t.priceId),
+			(feed) => {
+				const tokenName = tokens.find((t) => t.priceId.toLowerCase() === '0x'.concat(feed.id))!.name;
 
-        const _price = parsePriceToIPythPrice(feed);
+				const _price = parsePriceToIPythPrice(feed);
 
-        setPreviousPriceFeed((cur) => {
-          if (cur[tokenName] && cur[tokenName].gt(BigNumber.from(0)))
-            return cur;
-          return {
-            ...cur,
-            [tokenName]: _price,
-          };
-        });
+				setPreviousPriceFeed((cur) => {
+					if (cur[tokenName] && cur[tokenName].gt(BigNumber.from(0))) return cur;
+					return {
+						...cur,
+						[tokenName]: _price,
+					};
+				});
 
-        setPriceFeed((cur) => {
-          return {
-            ...cur,
-            [tokenName]: _price,
-          };
-        });
-      }
-    );
+				setPriceFeed((cur) => {
+					return {
+						...cur,
+						[tokenName]: _price,
+					};
+				});
+			},
+		);
 
-    return () => {
-      connection.closeWebSocket();
-    };
-    // eslint-disable-next-line
-  }, []);
+		return () => {
+			connection.closeWebSocket();
+		};
+		// eslint-disable-next-line
+	}, []);
 
-  return [priceFeed, previousPriceFeed];
+	return [priceFeed, previousPriceFeed];
 };
 
 const parsePriceToIPythPrice = (priceFeed: PriceFeed): BigNumber => {
-  const _price = priceFeed.getPriceUnchecked();
+	const _price = priceFeed.getPriceUnchecked();
 
-  const price = BigNumber.from(_price.price);
-  const expoToE30 = BigNumber.from(10).pow(30 + _price.expo);
+	const price = BigNumber.from(_price.price);
+	const expoToE30 = BigNumber.from(10).pow(30 + _price.expo);
 
-  return price.mul(expoToE30);
+	return price.mul(expoToE30);
 };
